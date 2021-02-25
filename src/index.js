1aa1e9b05dcf2f783e69fc98e4ab0b9352152c76
EthanMarkham <markham.ethan@gmail.com>
2021-02-24 20:46:45 -0500

    sdfsd

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-var fs = require('fs');
-
 function Square(props) {
   return (
     <button 
       className="square" 
       onClick={props.onClick}
     >
-    <img src={props.image} alt={props.value}></img>
+    <img 
+      src={props.image} 
+      alt={props.value}
+      className="image"
+      ></img>
     </button>
   );
 }
@@ -23,6 +25,7 @@ class Board extends React.Component {
       image={this.props.squares[i].image}
       //this doesnt need to be called onclick just comes in handy 
       onClick= {()=> this.props.onClick(i)}
+      key={i}
     />
     )
   }
@@ -48,21 +51,29 @@ class Board extends React.Component {
 
 class Game extends React.Component {
   constructor(props){
-    let answerSquares = generateBoard(),
-    defaultSquares = Array(answerSquares.length).fill({value:"*", image:"./public/Cards/back.PNG"})
-
     super(props)
     this.state = {
-      answerSquares: answerSquares.slice(),
-      currentSquares: defaultSquares.slice(),
-      defaultSquares: defaultSquares.slice(),
+      answerSquares: [],
+      currentSquares: [],
+      defaultSquares: [],
       cardsShown: 0,
       lastGuess: -1,
       round: 0,
       message: "Guess a square"
     }
   }
+  componentDidMount(){
+    fetch("http://localhost:3001/api/v1/gameBoard").then(response=> response.json()).then((data) => {
+      let defaultSquares = Array(data.length).fill({value:"*", image:"http://localhost:3001/cards/back.PNG"})
+      console.log(data);
+      this.setState({
+        answerSquares: data,
+        currentSquares: defaultSquares.slice(0),
+        defaultSquares: defaultSquares.slice(0),
+      })
 
+    })
+  }
   handleClick(i){
     let currentSquares = this.state.currentSquares,
         defaultSquares = this.state.defaultSquares,
@@ -70,24 +81,21 @@ class Game extends React.Component {
         lastGuess = this.state.lastGuess,
         round = this.state.round,
         message = "Guess a square"
+  console.log(defaultSquares)
 
    //if square or winner set do nothing
     if (this.state.waiting) return
     //if square is already set do nothing
     if (currentSquares[i].value !== "*" || "*" !== defaultSquares[i].value) return
-    
-
-    if (arrayEquals(defaultSquares, this.state.answerSquares)) {
-      console.log("t")
-      return
-    }
+    //if game over do nothing
+    if (arrayEquals(defaultSquares, this.state.answerSquares)) return
 
     //if two cards match check for winner and flip over
     if (cardsShown === 2) {
       currentSquares = defaultSquares.slice(0)
       cardsShown = 0
       round = round + 1
-      message = "2"
+      message = "Try Again!"
     }
 
     currentSquares[i] = this.state.answerSquares[i]
@@ -97,12 +105,11 @@ class Game extends React.Component {
     if (lastGuess === currentSquares[i].value && cardsShown === 2) {
       //record new match in default values
       defaultSquares = currentSquares.slice(0)
-      message = "1"
+      message = "Nice Match!"
     }
     if (arrayEquals(defaultSquares, this.state.answerSquares)) {
-      message = "24"
+      message = "Good job!"
     }
-
     lastGuess = currentSquares[i].value
 
     this.setState({
@@ -128,7 +135,7 @@ class Game extends React.Component {
       </div>
       <div className="game-info">
           <div>{this.state.message}</div>
-          <ol>{this.state.round}</ol>
+          <ol>Guesses: {this.state.round}</ol>
       </div>
       </div>
       
@@ -143,21 +150,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-
-//helper functions
-function generateBoard(){
-  let   pair1dir = fs.DocumentDirectoryPath + '/public/Cards/paired/1',
-        pair2dir = fs.DocumentDirectoryPath + '/public/Cards/paired/1'
-
-  fs.readdir(pair1dir).then((pair1files) =>{
-    fs.readdir(pair2dir).then((pair2files, pair1files) => {
-        let shuffledArray = [...Array(pair1files).keys()].flatMap(i => [{value: i, image: pair1files[i]}, {value: i, image: pair2files[i]}]).sort(() => Math.random() - 0.5) //make new array based off size -> duplicate all values for pairs -> randomize order
-        return shuffledArray
-      })
-    })
-
-}
-
 function arrayEquals(a, b) {
   return Array.isArray(a) &&
     Array.isArray(b) &&
@@ -169,3 +161,4 @@ function arrayEquals(a, b) {
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 //reportWebVitals(console.log);
+
